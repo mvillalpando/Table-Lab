e2af046b68f2b45fead5d459957702c889732d3b
Mariela Villalpando <mvillalpando@marielas-mac.local>
2016-06-24 09:02:11 -0700

    Initial Commit

---
+++ Table Lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table Lab
+//
+//  Created by Mariela Villalpando on 6/24/16.
+//  Copyright © 2016 OG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Table Lab/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Table Lab
+//
+//  Created by Mariela Villalpando on 6/24/16.
+//  Copyright © 2016 OG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
